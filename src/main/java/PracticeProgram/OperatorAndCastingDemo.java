c75dd00399ef233b6a8c8aed1bc53df5346bd818
siva <126542312+rajansiva0@users.noreply.github.com>
2025-07-27 22:17:22 +0530

    java practice 2

---
+++ src/main/java/PracticeProgram/OperatorAndCastingDemo.java
@@ -0,0 +1,32 @@
+package PracticeProgram;
+
+public class OperatorAndCastingDemo {
+    public static void main(String[] args){
+
+        System.out.println("Arithmetic Operation");
+
+
+        double a = 10.0;
+        double b = 20.0;
+        double sum = a + b;
+        System.out.println("Sum: " + sum);
+
+
+        double c = 10.0;
+        double d = 20.0;
+        double dif = c - d;
+        System.out.println("Difference:" +dif);
+
+
+        double e = 10.0;
+        double f = 20.0;
+        double pro = e * f;
+        System.out.println("Product:" +pro);
+
+
+        double g = 10.0;
+        double h = 20.0;
+        double quo = g / h;
+        System.out.println("Quotient:" +quo);
+    }
+}
