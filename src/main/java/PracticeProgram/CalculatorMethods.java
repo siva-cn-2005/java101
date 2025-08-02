9a3defae4c5af6936732218afcfb5959c650c429
siva <126542312+rajansiva0@users.noreply.github.com>
2025-08-02 19:18:03 +0530

    CalculatorMethods

---
+++ src/main/java/PracticeProgram/CalculatorMethods.java
@@ -0,0 +1,20 @@
+package PracticeProgram;
+
+public class CalculatorMethods {
+    public static void main(String[] args){
+        System.out.println("Hello, Alice!");
+
+        int a = 4;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("Sum of 4 and 7: " +sum);
+
+        int num1 = 4;
+        boolean isEven1 = num1 % 2 == 0;
+        System.out.println("4 is even; " +isEven1);
+
+        int num2 = 7;
+        boolean isodd2 = num2 % 1 == 0;
+        System.out.println("7 is odd: " + isodd2);
+    }
+}
